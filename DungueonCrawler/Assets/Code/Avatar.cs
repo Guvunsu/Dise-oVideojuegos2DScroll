f387134ff9cb54bcef64deed8c6952161e6b0bb5
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-09-04 13:30:59 -0600

    pioujnu

---
+++ DungueonCrawler/Assets/Code/Avatar.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Garyk.FiniteStateMachine {
+
+    public enum PlayerIndex {
+        one = 0, two = 1, three = 2, four = 3
+
+    }
+
+
+    public class Avatar : MonoBehaviour {
+        // Start is called before the first frame update
+        public PlayerIndex Index;
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+    }
+}
+}
