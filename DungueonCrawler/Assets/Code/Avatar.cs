b698bf83a234c1c1e96d239ca38069c5fa46fb09
Guvunsu <gavryk_fray_rojas@hotmail.com>
2024-09-06 13:29:13 -0600

    clase pasada

--- DungueonCrawler/Assets/Code/Avatar.cs
+++ DungueonCrawler/Assets/Code/Avatar.cs
@@ -1,27 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-namespace Garyk.FiniteStateMachine {
+namespace SotomaYorch.FiniteStateMachine {
+    #region Enums
 
     public enum PlayerIndex {
-        one = 0, two = 1, three = 2, four = 3
-
+        ONE = 0,  //Player Input start with index 0
+        TWO = 1,
+        THREE = 2,
+        FOUR = 3
     }
 
+    #endregion
 
     public class Avatar : MonoBehaviour {
-        // Start is called before the first frame update
-        public PlayerIndex Index;
+        #region Knobs
+
+        public PlayerIndex playerIndex;
+
+        #endregion
+
+        #region RuntimeVariables
+
+        protected Rigidbody _rigidbody;
+        protected Vector3 _moveDirection;
+
+        #endregion
+
+        #region UnityMethods
+
         void Start() {
-
+            _rigidbody = GetComponent<Rigidbody>();
         }
 
-        // Update is called once per frame
         void Update() {
-
+            _rigidbody.velocity = _moveDirection;
         }
 
+        #endregion
+
+        #region PublicMethods
+
+        public void OnMOVE(InputAction.CallbackContext value) {
+            //Input.GetKeyDown(KeyCode.A);
+            if (value.performed) {
+                //It could be wether WASD, arrows, numpad, left thumbstick or right thumbstick
+                _moveDirection = new Vector3(
+                    value.ReadValue<Vector2>().x,
+                    0.0f,
+                    value.ReadValue<Vector2>().y
+                    );
+            }
+            //Input.GetKeyUp(KeyCode.A);
+            else if (value.canceled) {
+                _moveDirection = Vector3.zero;
+            }
+        }
+
+        #endregion
     }
 }
-}
+
--- DungueonCrawler/Assets/Code/ControllerInputHandler.cs
+++ DungueonCrawler/Assets/Code/ControllerInputHandler.cs
@@ -2,24 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class ControllerInputHandler : MonoBehaviour {
-    // Start is called before the first frame update
-    void Start() {
 
+namespace SotomaYorch.FiniteStateMachine {
+    public class ControllerInputHandler : MonoBehaviour {
+        #region RuntimeVariables
+
+        protected PlayerInput _playerInput;
+        protected Avatar[] _allAvatarsInTheLevel;
+        protected Avatar _avatarOfTheSameIndex;
+
+        #endregion
+
+
+        void Start() {
+            _playerInput = GetComponent<PlayerInput>();
+            //_playerInput.playerIndex;
+
+            _allAvatarsInTheLevel = GameObject.FindObjectsOfType<Avatar>(true);
+            foreach (Avatar value in _allAvatarsInTheLevel) {
+                //Cast of a variable
+                //PlayerIndex (enum) -> int
+                //(int)value.playerIndex
+                if ((int)value.playerIndex == _playerInput.playerIndex) {
+                    //we found the avatar which has the same player index
+                    //as of this Player Input's Controller
+                    _avatarOfTheSameIndex = value;
+                    _avatarOfTheSameIndex.gameObject.SetActive(true);
+                }
+            }
+        }
+
+        #region InputCallbackMethods
+
+        public void OnMOVE(InputAction.CallbackContext value) {
+            _avatarOfTheSameIndex.OnMOVE(value);
+        }
+
+        #endregion
     }
-
-    // Update is called once per frame
-    void Update() {
-
-    }
-    #region InputCallBackMethods
-
-    public void OnMove() {
-
-
-
-    }
-
-    #endregion
-   
 }
